05673cc1cd39e34a4b89e6c5633f88cb9204b0df
Bhim magar <shokokomichan098@gmail.com>
2026-01-07 22:22:19 +0545

    the third push ('the fibonacci long push ")

--- 2026Journal/Months/Jan/C/function.c
+++ 2026Journal/Months/Jan/C/function.c
@@ -4,15 +4,22 @@
 # include <stdio.h>
 int fibonacciSequence(int num);
 int main () {
-    int n; 
+    int a = 0,b = 1 ,c = 0, n; 
     printf("Enter the number : ");
     scanf("%d", &n);
     // printf("Fibonacci of %d is %d\n", n, fibonacciSequence(n));
-
+    printf("The fibonacci suquence %d is  %d is : ", a,b);
+    c = a + b;
+   
+    while (c <= n){
+        printf("%d", c);
+        a = b;
+        b = c; 
+        c = a + b;
+    }
     
     return 0;
 }
-
 
 // Fibonacci sequence... By usin the C. 
 int fibonacciSequence(int num){
